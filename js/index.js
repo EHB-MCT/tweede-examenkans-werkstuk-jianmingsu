070dc9f1d9ff6041e55457814fdf4e58f05a7eac
Jianming Su <jianming.su@student.ehb.be>
2021-08-13 18:36:42 +0200

    returns array artikelen

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+async function getData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'GET'
+    });
+    return response.json();
+}
+
+getData('https://thecrew.cc/news/read.php')
+    .then(data => {
+        console.log(data);
+    });
+
+
+
