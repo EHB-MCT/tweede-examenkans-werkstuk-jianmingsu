b620730c2000f4be91f0136b9b96a18ecf97aba4
Jianming Su <jianming.su@student.ehb.be>
2021-08-13 19:16:50 +0200

    klasse gemaakt

--- js/index.js
+++ js/index.js
@@ -12,5 +12,62 @@ getData('https://thecrew.cc/news/read.php')
         console.log(data);
     });
 
+class Artikels {
+    constructor(articleID, articleTitle, articleContent, articleImageURL, articleLikes, articleDate) {
+        this._articleID = articleID,
+        this._articleTitle = articleTitle,
+        this._articleContent = articleContent,
+        this._articleImageURL = articleImageURL,
+        this._articleLikes = articleLikes,
+        this._articleDate = articleDate
+    }
 
+    get articleID() {
+        return this._articleID
+    }
+
+    set articleID(id) {
+        this._articleID = id
+    }
+
+    get articleTitle() {
+        return this._articleTitle
+    }
+
+    set articleTitle(title) {
+        this._articleTitle = title
+    }
+
+    get articleContent() {
+        return this._articleContent
+    }
+
+    set articleContent(content) {
+        this._articleContent = content
+    }
+
+    get articleImageURL() {
+        return this._articleImageURL
+    }
+
+    set articleImageURL(url) {
+        this._articleImageURL = url
+    }
+
+    get articleLikes() {
+        return this._articleLikes
+    }
+
+    set articleLikes(likes) {
+        this._articleLikes = likes
+    }
+
+    get articleDate() {
+        return this._articleDate
+    }
+
+    set articleDate(date) {
+        this._articleDate = date
+    }
+}
 
